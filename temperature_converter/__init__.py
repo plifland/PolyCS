e21222e6d3c6c0e1b23ab3532a6477edcfd51958
plifland <peter.lifland@gmail.com>
2026-07-15 23:49:24 -0700

    adding round() to temperature converter

--- temperature_converter/__init__.py
+++ temperature_converter/__init__.py
@@ -7,10 +7,15 @@ def exists():
 
 @check50.check(exists)
 def testzero():
-    """input of 0 yields output of Fahrenheit temperature: 32.0"""
-    check50.run("python3 temperature_converter.py").stdin("0", prompt=True).stdout("Fahrenheit temperature: 32.0").exit()
+    """input of 0 yields output of Fahrenheit temperature: 32"""
+    check50.run("python3 temperature_converter.py").stdin("0", prompt=True).stdout("Fahrenheit temperature: 32").exit()
 
 @check50.check(exists)
-def testfortytwo():
-    """input of 42 yields output of Fahrenheit temperature: 107.6"""
-    check50.run("python3 temperature_converter.py").stdin("42", prompt=True).stdout("Fahrenheit temperature: 107.6").exit()
+def testroundup():
+    """input of 42 yields output of Fahrenheit temperature: 108"""
+    check50.run("python3 temperature_converter.py").stdin("42", prompt=True).stdout("Fahrenheit temperature: 108").exit()
+
+@check50.check(exists)
+def testrounddown():
+    """input of 9 yields output of Fahrenheit temperature: 48"""
+    check50.run("python3 temperature_converter.py").stdin("9", prompt=True).stdout("Fahrenheit temperature: 48").exit()
