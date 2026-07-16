852b182f3cc2aadc3acfcf87983dcb74a9caa28b
plifland <peter.lifland@gmail.com>
2026-07-15 23:40:12 -0700

    added a non-checking version of personalized_ads

--- personalized_ads/__init__.py
+++ personalized_ads/__init__.py
@@ -2,11 +2,6 @@ import check50
 
 @check50.check()
 def exists():
-    """hello_world.py exists"""
-    check50.exists("hello_world.py")
-
-@check50.check(exists)
-def testhello():
-    """output is Hello, world!"""
-    check50.run("python3 hello_world.py").stdout("Hello, world!").exit()
+    """personalized_ads.py exists"""
+    check50.exists("personalized_ads.py")
 
