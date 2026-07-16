b9fab600484f5982e00fa708f64682cf2786e527
plifland <peter.lifland@gmail.com>
2026-07-16 00:20:30 -0700

    added checks using out of range values to number checker

--- number_guesser/__init__.py
+++ number_guesser/__init__.py
@@ -5,3 +5,27 @@ def exists():
     """number_guesser.py exists"""
     check50.exists("number_guesser.py")
 
+  @check50.check(exists)
+def test_too_low():
+    """Guessing too low on the first guess says 'Too low'"""
+    check50.run("python3 number_guesser.py").stdin("0", prompt=True).stdout(".*[Tt]oo low.*", regex=True).kill()
+
+@check50.check(exists)
+def test_too_high():
+    """Guessing too high on the first guess says 'Too high'"""
+    check50.run("python3 number_guesser.py").stdin("11", prompt=True).stdout(".*[Tt]oo high.*", regex=True).kill()
+
+@check50.check(exists)
+def test_too_low2():
+    """Guessing too low on the second guess says 'Too low'"""
+    check50.run("python3 number_guesser.py").stdin("11", prompt=True).stdin("0", prompt=True).stdout(".*[Tt]oo low.*", regex=True).kill()
+
+@check50.check(exists)
+def test_too_high2():
+    """Guessing too high on the second guess says 'Too high'"""
+    check50.run("python3 number_guesser.py").stdin("0", prompt=True).stdin("11", prompt=True).stdout(".*[Tt]oo high.*", regex=True).kill()
+
+@check50.check(exists)
+def test_lose():
+    """Guessing wrong three times loses"""
+    check50.run("python3 number_guesser.py").stdin("0", prompt=True).stdin("0", prompt=True).stdin("0", prompt=True).stdout(".*[Ll]ose.*", regex=True).exit()
