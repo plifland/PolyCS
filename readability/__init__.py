6767cc0245c15e90e88c02db1402a5b9c77f5066
plifland <peter.lifland@gmail.com>
2026-07-17 09:09:18 -0700

    moved includes to every check

--- readability/__init__.py
+++ readability/__init__.py
@@ -4,26 +4,29 @@ import check50
 def exists():
     """readability.py exists."""
     check50.exists("readability.py")
-    check50.include("texts")
 
 @check50.check(exists)
 def multiple_sentences():
     """handles multiple sentences"""
+    check50.include("texts")
     check50.run("python3 readability.py texts/rowling_excerpt.txt").stdout("Grade\D+5", "Grade 5\n").exit(0)
 
 @check50.check(exists)
 def multiple_lines():
     """handles mulitple lines of input"""
+    check50.include("texts")
     check50.run("python3 readability.py texts/textbook_excerpt.txt").stdout("Grade\D+11", "Grade 11\n").exit(0)
 
 @check50.check(exists)
 def long_text():
     """handles a long text"""
+    check50.include("texts")
     check50.run("python3 readability.py texts/carroll.txt").stdout("Grade\D+7", "Grade 7\n").exit(0)
 
 @check50.check(exists)
 def short_gutenberg_text():
     """handles a short text from Gutenberg by avoiding reading under *** END"""
+    check50.include("texts")
     output = check50.run("python3 readability.py texts/potter.txt").stdout("Grade\D+7", "Grade 7\n")
     help = None
     if output == "Grade 11":
