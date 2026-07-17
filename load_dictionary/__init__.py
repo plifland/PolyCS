94259d0f2ce0b4af10299a315c4af23ee97b9277
plifland <peter.lifland@gmail.com>
2026-07-17 15:13:06 -0700

    load_dict v1

---
+++ load_dictionary/__init__.py
@@ -0,0 +1,23 @@
+import check50
+
+@check50.check()
+def exists():
+    """load_dictionary.py exists."""
+    check50.exists("load_dictionary.py")
+
+@check50.check(exists)
+def load_small():
+    """successfully loads a small dictionary"""
+    check50.include("small")
+    check50.run("python3 load_dictionary.py small/dict.txt").stdout(open("small/out.txt").read()).exit(0)
+
+@check50.check(exists)
+def load_large():
+    """successfully loads a large dictionary"""
+    check50.include("large")
+    check50.run("python3 load_dictionary.py large/dict.txt").stdout(open("large/out.txt").read()).exit(0)
+
+@check50.check(exists)
+def test_reject_nofile():
+    """demands a file passed in via command line"""
+    check50.run("python3 load_dictionary.py").exit(1)
