a16a3d873b90a4ddb69663c5b02900aae439817a
plifland <peter.lifland@gmail.com>
2026-07-17 12:58:47 -0700

    game_of_life checks v1, copied from automata_board

---
+++ game_of_life/__init__.py
@@ -0,0 +1,43 @@
+import check50
+
+from collections import Counter
+from re import match
+
+@check50.check()
+def exists():
+    """game_of_life.py exists."""
+    check50.exists("game_of_life.py")
+    check50.include("boards")
+
+@check50.check(exists)
+def test_blinker():
+    """Prints the tiny 3x3 Blinker board"""
+    board = check50.run("python3 game_of_life.py boards/blinker.txt").stdout()
+    check_board(board, "boards/blinker.txt")
+
+@check50.check(exists)
+def test_pulsar():
+    """Prints the 17x17 Pulsar board"""
+    board = check50.run("python3 game_of_life.py boards/pulsar.txt").stdout()
+    check_board(board, "boards/pulsar.txt")
+
+@check50.check(exists)
+def test_spaceship():
+    """Prints the rectangular Heavy Weight Spaceship board"""
+    board = check50.run("python3 game_of_life.py boards/heavyWeightSpaceship.txt").stdout()
+    check_board(board, "boards/heavyWeightSpaceship.txt")
+
+def check_board(board, source):
+    # Get the two characters likely used to draw
+    char_cnts = Counter(board.replace("\n", ""))
+
+    # Replace the user characters with 0,1 like the source file
+    board = board.replace(char_cnts.most_common(2)[0][0], "0")
+    board = board.replace(char_cnts.most_common(2)[1][0], "1")
+
+    expected = open(source, encoding="utf-8-sig").read()
+    help = "\n\tIf you changed the looks of your board dramatically, it might not match and that's ok!"
+    if not match(expected, board):
+        if len(expected) == len(board) - 1:
+          help = "\n\tWatch out - the text file has a strange whitespace character that you are turning into a cell!\n\tMake sure you encode only 0s and 1s\n"
+        raise check50.Mismatch(expected, board, help=help)
