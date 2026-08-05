e2036b0f4711b582a6cc39af012182a93f09a8e0
plifland <peter.lifland@gmail.com>
2026-08-05 14:12:29 -0700

    temperature converter regex fix

--- TemperatureConverter/__init__.py
+++ TemperatureConverter/__init__.py
@@ -7,15 +7,15 @@ def exists():
 
 @check50.check(exists)
 def testzero():
-    """input of 0 yields output of Fahrenheit temperature: 32"""
-    check50.run("java TemperatureConverter.java").stdin("0").stdout("Fahrenheit temperature: 32").exit()
+    """input of 0 yields output of 32"""
+    check50.run("java TemperatureConverter.java").stdin("0").stdout("32").exit()
 
 @check50.check(exists)
 def testtruncate():
-    """input of 42 yields output of Fahrenheit temperature: 107"""
-    check50.run("java TemperatureConverter.java").stdin("42").stdout("Fahrenheit temperature: 107").exit()
+    """input of 42 yields output of 107"""
+    check50.run("java TemperatureConverter.java").stdin("42").stdout("107").exit()
 
 @check50.check(exists)
 def testrounddown():
-    """input of 9 yields output of Fahrenheit temperature: 48"""
-    check50.run("java TemperatureConverter.java").stdin("9").stdout("Fahrenheit temperature: 48").exit()
+    """input of 9 yields output of 48"""
+    check50.run("java TemperatureConverter.java").stdin("9").stdout("48").exit()
