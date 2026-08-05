a7635a14509fa8851f5ee0fa3912df55da17ef45
plifland <peter.lifland@gmail.com>
2026-08-05 14:18:05 -0700

    getting rid of the fancy text files now that the pset has been simplified

--- PersonalizedHello/__init__.py
+++ PersonalizedHello/__init__.py
@@ -4,33 +4,13 @@ import check50
 def exists():
     """PersonalizedHello.java exists"""
     check50.exists("PersonalizedHello.java")
-    check50.include("Panther.txt", "Poly Panther.txt")
 
 @check50.check(exists)
 def test1():
     """handles a 1 word name correctly """
-    out = check50.run("java ./PersonalizedHello.java").stdin("Panther", timeout=10).stdout()
-    check_output(out, open("Panther.txt").read())
+    check50.run("java ./PersonalizedHello.java").stdin("Panther", timeout=10).stdout("Hello, Panther!")
 
 @check50.check(exists)
 def test2():
     """handles a name with a space correctly """
-    out = check50.run("java ./PersonalizedHello.java").stdin("Poly Panther", timeout=10).stdout()
-    check_output(out, open("Poly Panther.txt").read())
-
-
-def check_output(output, correct):
-    if output.splitlines() == correct.splitlines():
-        return
-
-    output = [line for line in output.splitlines() if line != ""]
-    correct = correct.splitlines()
-
-    help = None
-    if len(output) == len(correct):
-        if all(ol.rstrip() == cl for ol, cl in zip(output, correct)):
-            help = output.splitlines()
-        elif all(ol[1:] == cl for ol, cl in zip(output, correct)):
-            help = "are you printing an additional character at the beginning of each line?"
-
-    raise check50.Mismatch(correct, output, help=help)
+    check50.run("java ./PersonalizedHello.java").stdin("Poly Panther", timeout=10).stdout("Hello, Poly Panther!")
