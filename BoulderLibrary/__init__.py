0baa8934a27c80ff56ce7abb1f681286043251b2
plifland <peter.lifland@gmail.com>
2026-08-05 18:33:53 -0700

    forcing compile to run first!

--- BoulderLibrary/__init__.py
+++ BoulderLibrary/__init__.py
@@ -10,22 +10,22 @@ def compiles():
     """BoulderLibrary.java compiles"""
     check50.run("javac BoulderLibrary.java")
 
-@check50.check(exists)
+@check50.check(compiles)
 def testfancy():
     """Serves a fancy fundraiser ad for a boulder resident over 24"""
     check50.run("java ./BoulderLibrary").stdin("80301", prompt=True).stdin("40", prompt=True).stdout("Fancy fundraiser ad:.+").exit()
 
-@check50.check(exists)
+@check50.check(compiles)
 def test25():
     """Serves a fancy fundraiser ad for a boulder resident exactly 25"""
     check50.run("java BoulderLibrary").stdin("80302", prompt=True).stdin("25", prompt=True).stdout("Fancy fundraiser ad:.+").exit()
 
-@check50.check(exists)
+@check50.check(compiles)
 def testevent():
     """Serves a library event ad for a boulder resident under 25"""
     check50.run("java ./BoulderLibrary").stdin("80310", timeout=10).stdin("17").stdout("Library event ad:.+").exit()
 
-@check50.check(exists)
+@check50.check(compiles)
 def testtourist():
     """Serves a tourist ad for a non-boulder resident"""
     check50.run("java BoulderLibrary").stdin("91106", timeout=10).stdin("17").stdout("Tourist ad:.+").exit()
