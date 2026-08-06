ba120d5263ddd24934a8db290cbaf20dbdd59f59
plifland <peter.lifland@gmail.com>
2026-08-05 18:58:25 -0700

    exit in the compile

--- BoulderLibrary/__init__.py
+++ BoulderLibrary/__init__.py
@@ -8,7 +8,7 @@ def exists():
 @check50.check(exists)
 def compiles():
     """BoulderLibrary.java compiles"""
-    check50.run("javac BoulderLibrary.java")
+    check50.run("javac BoulderLibrary.java").exit()
 
 @check50.check(compiles)
 def testfancy():
