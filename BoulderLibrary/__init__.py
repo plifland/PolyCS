db37aada1ad11e29b5edf3029a846b9c90a3b83a
plifland <peter.lifland@gmail.com>
2026-08-05 18:30:09 -0700

    trying a relative path

--- BoulderLibrary/__init__.py
+++ BoulderLibrary/__init__.py
@@ -13,19 +13,19 @@ def compiles():
 @check50.check(exists)
 def testfancy():
     """Serves a fancy fundraiser ad for a boulder resident over 24"""
-    check50.run("java BoulderLibrary").stdin("80301").stdin("40").stdout("Fancy fundraiser ad:.+").exit()
+    check50.run("java ./BoulderLibrary").stdin("80301").stdin("40").stdout("Fancy fundraiser ad:.+").exit()
 
 @check50.check(exists)
 def test25():
     """Serves a fancy fundraiser ad for a boulder resident exactly 25"""
-    check50.run("java BoulderLibrary").stdin("80302").stdin("25").stdout("Fancy fundraiser ad:.+").exit()
+    check50.run("java ./BoulderLibrary").stdin("80302").stdin("25").stdout("Fancy fundraiser ad:.+").exit()
 
 @check50.check(exists)
 def testevent():
     """Serves a library event ad for a boulder resident under 25"""
-    check50.run("java BoulderLibrary").stdin("80310").stdin("17").stdout("Library event ad:.+").exit()
+    check50.run("java ./BoulderLibrary").stdin("80310").stdin("17").stdout("Library event ad:.+").exit()
 
 @check50.check(exists)
 def testtourist():
     """Serves a tourist ad for a non-boulder resident"""
-    check50.run("java BoulderLibrary").stdin("91106").stdin("17").stdout("Tourist ad:.+").exit()
+    check50.run("java ./BoulderLibrary").stdin("91106").stdin("17").stdout("Tourist ad:.+").exit()
