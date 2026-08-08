53600340f253ad7ed7bc527836cb1e87feeb9bd4
plifland <peter.lifland@gmail.com>
2026-08-08 16:26:54 -0700

    rearranging checks

--- CookieJar/__init__.py
+++ CookieJar/__init__.py
@@ -5,15 +5,15 @@ def exists_jar():
     """CookieJar.java exists."""
     check50.exists("CookieJar.java")
 
-@check50.check()
-def exists_client():
-    """MysteriousBlueClient.java exists."""
-    check50.exists("MysteriousBlueClient.java")
-
 @check50.check(exists_jar)
 def compiles_jar():
     """CookieJar.java compiles"""
     check50.run("javac CookieJar.java").exit(0)
+
+@check50.check()
+def exists_client():
+    """MysteriousBlueClient.java exists."""
+    check50.exists("MysteriousBlueClient.java")
 
 @check50.check(exists_client)
 def compiles_client():
